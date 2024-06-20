091ab2c496c67deb97d9fded9bbb7582aac0bd2e
Sahil3982 <sahilpatel3982@gmail.com>
2024-06-20 10:08:22 +0530

    Update Banner.jsx

--- frontend/src/Components/Banner.jsx
+++ frontend/src/Components/Banner.jsx
@@ -2,7 +2,12 @@ import React from 'react'
 
 const Banner = () => {
   return (
-    <div className='section-container'>Banner</div>
+    <div className='section-container bg-gradient-to-r from-[#FAFAFA] from-0% to-[#FCFCFC] to-100%'>
+      <div className='py-24 flex justify-between'>
+        <div className=''>Left</div>
+        <div className=''>Right</div>
+      </div>
+    </div>
   )
 }
 
