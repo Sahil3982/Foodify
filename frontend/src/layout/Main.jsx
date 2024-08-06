ac581c224f971179dd97843742f27c69d3de93e1
Sahil3982 <sahilpatel3982@gmail.com>
2024-08-06 23:22:02 +0530

    Some Components are created

--- frontend/src/layout/Main.jsx
+++ frontend/src/layout/Main.jsx
@@ -1,7 +1,13 @@
 import React from 'react'
-
+import Footer from '../Components/Footer'
+import NavBar from '../Components/NavBar'
+import { Outlet } from 'react-router'
 export const Main = () => {
   return (
-    <div>Main</div>
+    <>
+    <NavBar> </NavBar>
+    <Outlet />
+    <Footer></Footer>
+    </>
   )
 }
